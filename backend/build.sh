d12245c53a4386bd9da6798f451b9b5b4b71e666
bleuciel27 <lavenderadiation@gmail.com>
2025-01-22 11:23:06 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,3 +2,12 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+
+# Add build commands
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
