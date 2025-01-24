013f0d9fefa3b3861f905d73335e8d4c1ddba1de
bleuciel27 <lavenderadiation@gmail.com>
2025-01-24 09:37:47 +0530

    Delete marketplace-frontend/assets/js directory

--- marketplace-frontend/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    API_URL: 'http://localhost:8000/api',
-    BASE_URL: 'http://localhost:8000',
-    getImageUrl: function(imagePath) {
-        // Remove any leading slashes to avoid double slashes
-        const cleanPath = imagePath.replace(/^\//, '');
-        return `${this.BASE_URL}/${cleanPath}`;
-    }
-};
